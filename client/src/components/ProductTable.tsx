ee7fa7c57217d3fe98f718eff41c92ed3365c65f
Andrei Bautin <andreibautin94@gmail.com>
2023-05-06 14:39:23 -0600

    cart alignment

--- client/src/components/ProductTable.tsx
+++ client/src/components/ProductTable.tsx
@@ -7,74 +7,77 @@ import {
   ScrollArea,
 } from "@mantine/core";
 import { IconTrash } from "@tabler/icons-react";
-import { useState } from "react";
 import { CartProduct } from "../features/cart/cartSlice";
 import { useAppSelector } from "../app/hooks";
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    textAlign: "center",
+  },
+}));
 
 export function ProductTable() {
+  const { classes, cx } = useStyles();
+
   const cart = useAppSelector((state) => state.cart);
 
   const removeItem = (productId: number) => {};
 
   return (
-    <ScrollArea>
-      <Table sx={{ minWidth: 800 }} verticalSpacing="sm">
-        <thead>
+    <Table sx={{ minWidth: 800 }} verticalSpacing="sm">
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th>Total</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {cart.products.map((row: CartProduct) => (
           <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Quantity</th>
-            <th>Price</th>
-            <th>Total</th>
-            <th />
+            <td>
+              <Group>
+                <Avatar size={100} src={`/images/${row.product.imageName}`} />
+              </Group>
+            </td>
+            <td>
+              <Text fz="sm" c="dimmed" ta="left">
+                {row.product.productName}
+              </Text>
+            </td>
+            <td>
+              <Text fz="sm" c="dimmed" ta="left">
+                {row.quantity}
+              </Text>
+            </td>
+            <td>
+              <Text fz="sm" c="dimmed" ta="left">
+                {row.product.price}
+              </Text>
+            </td>
+            <td>
+              <Text fz="sm" c="dimmed" ta="left">
+                {row.product.price * row.quantity}
+              </Text>
+            </td>
+            <td>
+              <Group spacing={0} position="left">
+                <ActionIcon color="red">
+                  <IconTrash
+                    size="1rem"
+                    stroke={1.5}
+                    onClick={() => removeItem(row.product.id)}
+                  />
+                </ActionIcon>
+              </Group>
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {cart.products.map((row: CartProduct) => (
-            <tr>
-              <td>
-                <Group spacing="sm">
-                  <Avatar size={100} src={`/images/${row.product.imageName}`} />
-                  <Text fz="sm" fw={500}>
-                    {row.product.productName}
-                  </Text>
-                </Group>
-              </td>
-              <td>
-                <Text fz="sm" c="dimmed">
-                  {row.product.productName}
-                </Text>
-              </td>
-              <td>
-                <Text fz="sm" c="dimmed">
-                  {row.quantity}
-                </Text>
-              </td>
-              <td>
-                <Text fz="sm" c="dimmed">
-                  {row.product.price}
-                </Text>
-              </td>
-              <td>
-                <Text fz="sm" c="dimmed">
-                  {row.product.price * row.quantity}
-                </Text>
-              </td>
-              <td>
-                <Group spacing={0} position="right">
-                  <ActionIcon color="red">
-                    <IconTrash
-                      size="1rem"
-                      stroke={1.5}
-                      onClick={() => removeItem(row.product.id)}
-                    />
-                  </ActionIcon>
-                </Group>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </ScrollArea>
+        ))}
+      </tbody>
+    </Table>
   );
 }
