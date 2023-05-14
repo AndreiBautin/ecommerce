b32e23bdc33c2e8d82af0513bdeb6a7e9aa20389
Andrei Bautin <andreibautin94@gmail.com>
2023-05-14 11:11:14 -0600

    Empty cart message

--- client/src/components/CartTable.tsx
+++ client/src/components/CartTable.tsx
@@ -1,4 +1,12 @@
-import { Avatar, Table, Group, Text, ActionIcon } from "@mantine/core";
+import {
+  Avatar,
+  Table,
+  Group,
+  Text,
+  ActionIcon,
+  Container,
+  Title,
+} from "@mantine/core";
 import { IconTrash } from "@tabler/icons-react";
 import { CartProduct } from "../features/cart/cartSlice";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
@@ -17,86 +25,95 @@ export function CartTable() {
     currency: "USD",
   });
 
-  return (
-    <Table sx={{ minWidth: 800 }} verticalSpacing="sm">
-      <thead>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Quantity</th>
-          <th>Price</th>
-          <th>Total</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {cart.products.map((row: CartProduct) => (
+  if (cart.products.length > 0) {
+    return (
+      <Table sx={{ minWidth: 800 }} verticalSpacing="sm">
+        <thead>
           <tr>
-            <td>
-              <Group>
-                <Avatar size={100} src={`/images/${row.product.imageName}`} />
-              </Group>
-            </td>
-            <td>
-              <Text fz="sm" c="dimmed" ta="left">
-                {row.product.productName}
-              </Text>
-            </td>
-            <td>
-              <Text fz="sm" c="dimmed" ta="left">
-                {row.quantity}
-              </Text>
-            </td>
-            <td>
-              <Text fz="sm" c="dimmed" ta="left">
-                {formatter.format(
-                  row.product.price * (1 - row.product.discount)
-                )}
-              </Text>
-            </td>
-            <td>
-              <Text fz="sm" c="dimmed" ta="left">
-                {formatter.format(
-                  row.product.price * (1 - row.product.discount) * row.quantity
-                )}
-              </Text>
-            </td>
-            <td>
-              <Group spacing={0} position="left">
-                <ActionIcon color="red">
-                  <IconTrash
-                    size="1rem"
-                    stroke={1.5}
-                    onClick={() => removeItem(row.product.id)}
-                  />
-                </ActionIcon>
-              </Group>
-            </td>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Total</th>
+            <th />
           </tr>
-        ))}
-      </tbody>
-      <tfoot>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td>
-            {" "}
-            <Text fz="sm" c="dimmed" ta="left">
-              Grand Total:{" "}
-              {formatter.format(
-                cart.products.reduce(function (acc, obj) {
-                  var priceWithDiscount =
-                    obj.product.price * (1 - obj.product.discount);
-                  return acc + priceWithDiscount * obj.quantity;
-                }, 0)
-              )}
-            </Text>
-          </td>
-          <td></td>
-        </tr>
-      </tfoot>
-    </Table>
+        </thead>
+        <tbody>
+          {cart.products.map((row: CartProduct) => (
+            <tr>
+              <td>
+                <Group>
+                  <Avatar size={100} src={`/images/${row.product.imageName}`} />
+                </Group>
+              </td>
+              <td>
+                <Text fz="sm" c="dimmed" ta="left">
+                  {row.product.productName}
+                </Text>
+              </td>
+              <td>
+                <Text fz="sm" c="dimmed" ta="left">
+                  {row.quantity}
+                </Text>
+              </td>
+              <td>
+                <Text fz="sm" c="dimmed" ta="left">
+                  {formatter.format(
+                    row.product.price * (1 - row.product.discount)
+                  )}
+                </Text>
+              </td>
+              <td>
+                <Text fz="sm" c="dimmed" ta="left">
+                  {formatter.format(
+                    row.product.price *
+                      (1 - row.product.discount) *
+                      row.quantity
+                  )}
+                </Text>
+              </td>
+              <td>
+                <Group spacing={0} position="left">
+                  <ActionIcon color="red">
+                    <IconTrash
+                      size="1rem"
+                      stroke={1.5}
+                      onClick={() => removeItem(row.product.id)}
+                    />
+                  </ActionIcon>
+                </Group>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              {" "}
+              <Text fz="sm" c="dimmed" ta="left">
+                Grand Total:{" "}
+                {formatter.format(
+                  cart.products.reduce(function (acc, obj) {
+                    var priceWithDiscount =
+                      obj.product.price * (1 - obj.product.discount);
+                    return acc + priceWithDiscount * obj.quantity;
+                  }, 0)
+                )}
+              </Text>
+            </td>
+            <td></td>
+          </tr>
+        </tfoot>
+      </Table>
+    );
+  }
+  return (
+    <Container px="lg" py="lg">
+      Empty
+    </Container>
   );
 }
